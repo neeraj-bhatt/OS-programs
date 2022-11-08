46ccd10c994d65ca9a64993fc0bc58cd47668219
neerajbhatt <neerajbhatt264@gmail.com>
2022-11-08 17:57:54 +0530

    Fork program added

---
+++ Ques1c.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+void parentWaitForChild()
+{
+  pid_t pidFork;
+  pidFork = fork();
+  if(pidFork < 0)
+    cout << "\nError in fork...";
+  else if(pidFork == 0)
+    cout << "\nChild executed\nProcess ID : " <<  getpid() << "\nParent Process ID : " << getppid() << endl;
+  else
+  {
+    wait(NULL);
+    cout << "\nParent executed\nProcess ID : " << getpid() << "\nParent Process ID : " << getppid() << endl;
+  }
+  return;
+}
+
+int main()
+{
+  parentWaitForChild();
+}
