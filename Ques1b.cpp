c65cc70f7d386d3b5dcde7459334f0bb2eec1762
neerajbhatt <neerajbhatt264@gmail.com>
2022-11-08 17:57:37 +0530

    program update

--- Ques1b.cpp
+++ Ques1b.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-#include <sys/wait.h>
 #include <unistd.h>
+
 using namespace std;
+
+void sameProgramDiffCode()
+{
+  pid_t pidFork;
+  pidFork = fork();
+  if(pidFork < 0)
+    cout << "\nError in fork...";
+  else if(pidFork == 0)
+    cout << "\nChild executed\nProcess ID : " <<  getpid() << "\nParent Process ID : " << getppid() << endl;
+  else
+    cout << "\nParent executed\nProcess ID : " << getpid() << "\nParent Process ID : " << getppid() << endl;
+  return;
+}
 
 int main()
 {
-    int a = fork();
-    if(a == 0)
-        cout << "Child Process." << endl;
-    else
-        cout << "Parent Process." << endl;
-
-    return 0;
+  sameProgramDiffCode();
 }
